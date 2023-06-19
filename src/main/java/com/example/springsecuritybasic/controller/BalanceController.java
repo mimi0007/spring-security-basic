b95781224faa9b3e0d766a28c8d6a5a7228ed6a5
mimi0007 <afsana.aziz@vivacomsolutions.com>
2023-06-19 17:04:25 +0600

    Initialize Controller Classes with Dummy Methods

--- src/main/java/com/example/springsecuritybasic/controller/BalanceController.java
+++ src/main/java/com/example/springsecuritybasic/controller/BalanceController.java
@@ -1,4 +1,14 @@
 package com.example.springsecuritybasic.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BalanceController {
+
+    @GetMapping("/myBalance")
+    public String getBalance() {
+        return "Balance Controller";
+    }
+
 }
